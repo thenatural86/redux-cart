4c76212901aba4aa1181ffcee6d8f110ee96c2df
thenatural86 <philip.smalls86@gmail.com>
2021-01-26 14:19:07 -0500

    remove item functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,20 @@
-import { CLEAR_CART, DECREASE, INCREASE } from './actions'
+import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from './actions'
 
 const reducer = (state, action) => {
   switch (action.type) {
     case CLEAR_CART:
       return { ...state, cart: [] }
+    case DECREASE:
+      console.log('decrease')
+    case INCREASE:
+      console.log('increase')
+    case REMOVE:
+      return {
+        ...state,
+        cart: state.cart.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      }
     default:
       return state
   }
