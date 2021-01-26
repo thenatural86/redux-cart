f743e49f8fa9ef60b6a36a4ca0274d38ecdc065d
thenatural86 <philip.smalls86@gmail.com>
2021-01-26 11:00:11 -0500

    dispatch action

--- src/App.js
+++ src/App.js
@@ -14,11 +14,17 @@ const initialStore = {
 
 const reducer = (state, action) => {
   console.log({ state, action })
+  if (action.type === 'DECREASE') {
+    console.log('yolo')
+    return { count: state.count - 1 }
+  }
   return state
 }
 
 const store = createStore(reducer, initialStore)
+store.dispatch({ type: 'DECREASE' })
 console.log(store.getState())
+
 function App() {
   // cart setup
 
