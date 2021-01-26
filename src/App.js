dd387fa46281f1c12d4c845a0beee605f927456b
thenatural86 <philip.smalls86@gmail.com>
2021-01-26 11:30:40 -0500

    actions file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import cartItems from './cart-items'
 // redux stuff
 
 import { createStore } from 'redux'
+import { DECREASE, INCREASE } from './actions'
 
 const initialStore = {
   count: 0,
@@ -15,27 +16,19 @@ const initialStore = {
 
 const reducer = (state, action) => {
   console.log({ state, action })
-  if (action.type === 'DECREASE') {
+  if (action.type === DECREASE) {
     return { ...state, count: state.count - 1 }
   }
-  if (action.type === 'INCREASE') {
+  if (action.type === INCREASE) {
     return { ...state, count: state.count + 1 }
   }
-  if (action.type === 'RESET') {
-    return { ...state, count: 0 }
-  }
-  if (action.type === 'CHANGE_NAME') {
-    return { ...state, name: 'lola' }
-  }
-
   return state
 }
 
 const store = createStore(reducer, initialStore)
-store.dispatch({ type: 'DECREASE' })
-store.dispatch({ type: 'INCREASE' })
-store.dispatch({ type: 'CHANGE_NAME' })
-store.dispatch({ type: 'RESET' })
+store.dispatch({ type: DECREASE })
+store.dispatch({ type: INCREASE })
+
 console.log(store.getState())
 
 function App() {
