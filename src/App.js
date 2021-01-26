bc2a5c149619ecf969f1c09ee36fce8259212ddc
thenatural86 <philip.smalls86@gmail.com>
2021-01-26 10:19:15 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+// components
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+// items
+import cartItems from './cart-items'
+// redux stuff
+
+import { createStore } from 'redux'
+
+const initialStore = {
+  count: 69,
+}
+
+const reducer = (state, action) => {
+  console.log({ state, action })
+  return state
+}
+
+const store = createStore(reducer, initialStore)
+console.log(store.getState())
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar cart={store.getState()} />
+      <CartContainer cart={cartItems} />
+    </main>
+  )
+}
+
+export default App
